7dfd6e6ae1691c1ab42f0145e2ac9ae13e292418
Cheyra <staiercm@gmail.com>
2019-02-13 14:38:32 -0500

    Updates

---
+++ precache-manifest.b1a0a0aefcb3830299ea721b2c7de11c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8cc2b04e0f05f41d23455481ac2b4853",
+    "url": "/Clicky-game/static/media/dragonballs.8cc2b04e.jpg"
+  },
+  {
+    "revision": "057d0eca7d6ab44d5ee9",
+    "url": "/Clicky-game/static/js/runtime~main.057d0eca.js"
+  },
+  {
+    "revision": "5c14bc8acd2e8508b16c",
+    "url": "/Clicky-game/static/js/main.5c14bc8a.chunk.js"
+  },
+  {
+    "revision": "70b62554493970a38f8b",
+    "url": "/Clicky-game/static/js/2.70b62554.chunk.js"
+  },
+  {
+    "revision": "5c14bc8acd2e8508b16c",
+    "url": "/Clicky-game/static/css/main.785f3ea0.chunk.css"
+  },
+  {
+    "revision": "a7c5ea7caee96d3f85a3efbe2b51d9dd",
+    "url": "/Clicky-game/index.html"
+  }
+];
